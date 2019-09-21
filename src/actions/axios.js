e468b4963e39126d881af7bf5f93e1b8e886d425
ashuarena <ashutosh.kumar@sendinblue.com>
2019-09-21 20:43:57 +0530

    app files

--- src/actions/axios.js
+++ src/actions/axios.js
@@ -1,7 +1,8 @@
 import { create } from 'axios';
 
 export const config = {
-	baseURL: 'http://ideaweaver.in/frazil-php'
+	baseURL: 'http://ideaweaver.in/frazil-php',
+	//baseURL: 'http://localhost/frazil-php'
 };
 const axiosInstance = create(config);
 
